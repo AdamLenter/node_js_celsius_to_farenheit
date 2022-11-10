70133ba2ffe85f9005eedb18ce455dd5aa294dca
Adam <adamlenter1@gmail.com>
2022-11-10 11:07:09 -0500

    Abstracted out temperature conversion function.

--- celsius-to-farenheit.js
+++ celsius-to-farenheit.js
@@ -1,8 +1,6 @@
-function celsiusToFahrenheit(celsius) {
-    return celsius * (9/5) + 32;
-}
+const converters = require('./converters.js');
  
 const celsiusInput = process.argv[2]; // Get the 3rd input from the argument list
-const fahrenheitValue = celsiusToFahrenheit(celsiusInput);
+const fahrenheitValue = converters.celsiusToFahrenheit(celsiusInput);
  
 console.log(`${celsiusInput} degrees Celsius = ${fahrenheitValue} degrees Fahrenheit`);
